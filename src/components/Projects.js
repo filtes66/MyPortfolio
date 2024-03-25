a9a3d6a2df869ca794c6818b819e7e89b8c5d525
philippe teisseyre <philippeteisseyre@philippes-MBP.lan>
2024-03-25 21:23:02 +0100

    Link replaced by a

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,10 +38,10 @@ function Projects () {
               </div>
               <div className="flex-1 bg-white p-6 flex flex-col justify-between">
                 <div className="flex-1">
-                  <Link to={project.projectUrl} className="block mt-2">
+                  <a href={project.projectUrl} className="block mt-2">
                     <p className="text-xl font-semibold text-gray-900">{project.title}</p>
                     <p className="mt-3 text-base text-gray-500">{project.description}</p>
-                  </Link>
+                  </a>
                 </div>
               </div>
             </div>
